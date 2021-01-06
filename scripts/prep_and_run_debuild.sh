Netronome/virtio-forwarder-dist

commit 621eab1e23c0ed059a99fc17a2d83c946e01b226

Add meson build support
From DPDK 20.11 going forward the use of "make" has been deprecated and
replaced with meson and ninja. This commit adds the necessary meson
build files and helper scripts to build and package virtio-forwarder
using meson

This commit also adds the ability to build "static" versions of
virtio-forwarder. These static packages contain a virtio-forwarder
binary that is statically linked to the DPDK libraries.

For example, to build a static .deb: (executed in the main
virtio-forwarder repo)
    meson -Ddeb=true -Dstatic=true build
    ninja -C build build_deb

Change-Id: Ice57bd722c59d5a6448e5cc93ec2c540a3360049
Signed-off-by: Heinrich Kuhn <heinrich.kuhn@netronome.com>
Reviewed-by: Louis Peens <louis.peens@netronome.com>
Signed-off-by: Simon Horman <simon.horman@netronome.com>

meson.build
@@ -1,0 +1,55 @@
+#   BSD LICENSE
+#
+#   Copyright(c) 2016-2020 Netronome.
+#   All rights reserved.
+#
+#   Redistribution and use in source and binary forms, with or without
+#   modification, are permitted provided that the following conditions
+#   are met:
+#
+#     * Redistributions of source code must retain the above copyright
+#       notice, this list of conditions and the following disclaimer.
+#     * Redistributions in binary form must reproduce the above copyright
+#       notice, this list of conditions and the following disclaimer in
+#       the documentation and/or other materials provided with the
+#       distribution.
+#     * Neither the name of Netronome nor the names of its
+#       contributors may be used to endorse or promote products derived
+#       from this software without specific prior written permission.
+#
+#   THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+#   "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+#   LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+#   A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+#   OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+#   SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+#   LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+#   DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+#   THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+#   (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+#   OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+if get_option('rpm')
+    prep_run_rpmbuild = find_program(join_paths('scripts', 'prep_and_run_rpmbuild.sh'))
+    if get_option('outdir') == 'outdir'
+        error('Please provide destination for RPM via the "-Doutdir" option')
+    else
+        outdir = get_option('outdir')
+    endif
+
+    if get_option('static')
+        run_target('build_rpm', command: [prep_run_rpmbuild, '-s', '-o', outdir])
+    else
+        run_target('build_rpm', command: [prep_run_rpmbuild, '-o', outdir])
+    endif
+endif
+
+if get_option('deb')
+    prep_run_debuild = find_program(join_paths('scripts', 'prep_and_run_debuild.sh'))
+
+    if get_option('static')
+        run_target('build_deb', command: [prep_run_debuild, '-s'])
+    else
+        run_target('build_deb', command: [prep_run_debuild])
+    endif
+endif

scripts/prep_and_run_debuild.sh
@@ -1,0 +1,118 @@
+#!/bin/bash
+
+#   BSD LICENSE
+#
+#   Copyright(c) 2016-2020 Netronome.
+#   All rights reserved.
+#
+#   Redistribution and use in source and binary forms, with or without
+#   modification, are permitted provided that the following conditions
+#   are met:
+#
+#     * Redistributions of source code must retain the above copyright
+#       notice, this list of conditions and the following disclaimer.
+#     * Redistributions in binary form must reproduce the above copyright
+#       notice, this list of conditions and the following disclaimer in
+#       the documentation and/or other materials provided with the
+#       distribution.
+#     * Neither the name of Netronome nor the names of its
+#       contributors may be used to endorse or promote products derived
+#       from this software without specific prior written permission.
+#
+#   THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+#   "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+#   LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+#   A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+#   OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+#   SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+#   LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+#   DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+#   THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+#   (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+#   OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+while getopts :s opt; do
+    case ${opt} in
+      s)
+        STATIC="-static"
+        CONFLICTS_WITH="Conflicts: virtio-forwarder"
+        MESON_STATIC="-Dstatic=true"
+        ;;
+    esac
+done
+
+STATIC=${STATIC:-""}
+CONFLICTS_WITH=${CONFLICTS_WITH:-"Conflicts:"}
+MESON_STATIC=${MESON_STATIC:-"-Dstatic=false"}
+DEBUILD_DIR="virtio-forwarder$STATIC"
+
+PKG_RELEASE=1
+DATE_STR=$(date --rfc-2822)
+DEBIAN_DISTRO=${DEBIAN_DISTRO:-"unstable"}
+
+TMP_BUILD="${MESON_BUILD_ROOT}"/_build/virtio-forwarder
+
+# Create scratch work directory
+mkdir -p "${TMP_BUILD}"
+
+# Copy source files from virtio-forwarder root directory to scratch area
+cd "${MESON_SOURCE_ROOT}"
+find . -maxdepth 1 -type f -regextype posix-extended \
+                -regex '.*(LICENSE|README.rst|README.md|meson.build|\.(c|h|in|proto|txt))' \
+                -exec cp "{}" "${TMP_BUILD}" \;
+
+find scripts/ -maxdepth 1 -type f -regextype posix-extended \
+                -regex '.*(meson.build|.*\.py)' \
+                -exec cp --parents "{}" "${TMP_BUILD}" \;
+
+cp --parents startup/virtioforwarder "${TMP_BUILD}"
+cp --parents startup/systemd/*.service "${TMP_BUILD}"
+cp --parents startup/systemd/meson* "${TMP_BUILD}"
+cp -r doc/ "${TMP_BUILD}"
+
+find startup/ -maxdepth 1 -type f -regextype posix-extended \
+                -regex '.*(meson.build|.*\.sh)' \
+                -exec cp --parents "{}" "${TMP_BUILD}" \;
+
+# Grab the correct version string from vrelay_version.h
+cd "${MESON_BUILD_ROOT}"/_build
+VERSION_VER_STRING=$(awk '/VIRTIO_FWD_VERSION/&&/define/&&!/SHASH/{count++;if (count<4) printf "%s.", $3; else print $3}' \
+    "${MESON_BUILD_ROOT}"/vrelay_version.h)
+
+DEBUILD_DIR="${MESON_BUILD_ROOT}"/_build/virtio-forwarder"${STATIC}"-"${VERSION_VER_STRING}"
+
+# Create a orig.tar.bz2 of the virtio-forwarder code
+mv virtio-forwarder "${DEBUILD_DIR}"
+tar cfjp virtio-forwarder"${STATIC}"_"${VERSION_VER_STRING}".orig.tar.bz2 \
+    virtio-forwarder"${STATIC}"-"${VERSION_VER_STRING}"
+
+# Grab the debian build files from the packaging repo
+cp -rf "${MESON_SOURCE_ROOT}"/packaging/debian "${DEBUILD_DIR}"
+
+# Populate versions
+sed -ri "s/__VRELAY_VERSION__/${VERSION_VER_STRING}/" \
+    "${DEBUILD_DIR}"/debian/changelog
+sed -ri "s/__PKG_RELEASE__/${PKG_RELEASE}/" \
+    "${DEBUILD_DIR}"/debian/changelog;
+
+cd "${DEBUILD_DIR}"
+
+# Make needed rules changes
+sed -ri "s/__MESON_STATIC__/${MESON_STATIC}/g" ./debian/rules
+
+# Make needed changelog changes
+sed -ri "s/__DEBIAN_DIST__/${DEBIAN_DISTRO}/g" ./debian/changelog
+sed -ri "s/__DATE__/${DATE_STR}/g" ./debian/changelog
+sed -ri "s/__STATIC__/${STATIC}/g" ./debian/changelog
+
+# Make needed control changes
+sed -ri "s/__STATIC__/${STATIC}/g" ./debian/control
+sed -ri "s/__CONFLICTS_WITH__/${CONFLICTS_WITH}/g" ./debian/control
+# For static builds we don't need DPDK packages
+if [ "${STATIC}" == "-static" ]; then
+  sed -i "s/ dpdk,//" ./debian/control
+  sed -i "/Recommends:/d" ./debian/control
+fi
+
+# Finally run debuild
+debuild --rootcmd=fakeroot -e PATH -e CFLAGS -e V -us -uc

debian/control
@@ -1,15 +1,31 @@
-Source: virtio-forwarder
+Source: virtio-forwarder__STATIC__
 Maintainer: Heinrich Kuhn <heinrich.kuhn@netronome.com>
 Section: net
-Build-Depends: debhelper (>= 11~), python-minimal, protobuf-c-compiler, libprotobuf-c-dev, libzmq3-dev, protobuf-compiler, python3-sphinx, libnuma-dev, libdpdk-dev, libpcap-dev, libxen-dev, meson (>= 0.47~), pkg-config, systemd
+Build-Depends:
+ debhelper (>= 11.1~),
+ python3-minimal,
+ protobuf-c-compiler,
+ libprotobuf-c-dev,
+ libzmq3-dev,
+ protobuf-compiler,
+ python3-sphinx,
+ libnuma-dev,
+ libdpdk-dev,
+ libpcap-dev,
+ libxen-dev,
+ meson (>= 0.50~),
+ pkg-config,
+ systemd,
+ dpdk-dev
 Standards-Version: 4.1.4
 Homepage: https://github.com/Netronome/virtio-forwarder
 Priority: optional
 
-Package: virtio-forwarder
+Package: virtio-forwarder__STATIC__
 Architecture: amd64 i386
-Depends: ${misc:Depends}, ${shlibs:Depends}, dpdk, python, python-zmq, python-protobuf
+Depends: ${misc:Depends}, ${shlibs:Depends}, dpdk, python, python3-zmq, python-protobuf
 Recommends: ${shlibs:Recommends}
+__CONFLICTS_WITH__
 Description: SR-IOV virtual function to virtio packet forwarder
  virtio-forwarder is a userspace networking application that forwards
  bi-directional traffic between SR-IOV virtual functions and virtio networking

debian/rules
@@ -12,7 +12,12 @@ include /usr/share/dpkg/default.mk
 PMD_PKGS := $(shell apt-cache depends dpdk | grep 'Recommends: librte' | cut -d':' -f2- | xargs | sed -e 's/ /, /g')
 
 %:
-	dh $@ --buildsystem=meson
+	dh $@
+
+override_dh_auto_configure:
+	dh_auto_configure -- \
+		--buildtype=debug \
+		__MESON_STATIC__
 
 override_dh_gencontrol:
 	dh_gencontrol -- -Vshlibs:Recommends="$(PMD_PKGS)"

scripts/prep_and_run_rpmbuild.sh
@@ -1,0 +1,121 @@
+#!/bin/bash
+
+#   BSD LICENSE
+#
+#   Copyright(c) 2016-2020 Netronome.
+#   All rights reserved.
+#
+#   Redistribution and use in source and binary forms, with or without
+#   modification, are permitted provided that the following conditions
+#   are met:
+#
+#     * Redistributions of source code must retain the above copyright
+#       notice, this list of conditions and the following disclaimer.
+#     * Redistributions in binary form must reproduce the above copyright
+#       notice, this list of conditions and the following disclaimer in
+#       the documentation and/or other materials provided with the
+#       distribution.
+#     * Neither the name of Netronome nor the names of its
+#       contributors may be used to endorse or promote products derived
+#       from this software without specific prior written permission.
+#
+#   THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+#   "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+#   LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+#   A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+#   OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+#   SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+#   LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+#   DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+#   THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+#   (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+#   OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+while getopts 'so:' opt; do
+    case ${opt} in
+      s)
+        STATIC="-static"
+        CONFLICTS_WITH="Conflicts: virtio-forwarder"
+        MESON_STATIC="-Dstatic=true"
+        ;;
+      o)
+        OUTDIR="$OPTARG"
+        ;;
+    esac
+done
+
+STATIC=${STATIC:-""}
+CONFLICTS_WITH=${CONFLICTS_WITH:-""}
+MESON_STATIC=${MESON_STATIC:-""}
+
+PKG_RELEASE=1
+DATE_STR=$(date +'%a %b %d %Y')
+TMP_BUILD="${MESON_BUILD_ROOT}"/_build/virtio-forwarder
+RPM_TOPDIR="${MESON_BUILD_ROOT}"/rpmbuild
+RPM_SPEC="${RPM_TOPDIR}"/SPECS/virtio-forwarder.spec
+
+# Create scratch work directory
+mkdir -p "${TMP_BUILD}"
+
+# Create rpmbuild directory structure
+mkdir -p "${RPM_TOPDIR}"/{BUILD,RPMS,SOURCES,SPECS,SRPMS}
+
+# Copy source files from virtio-forwarder root directory to scratch area
+cd "${MESON_SOURCE_ROOT}"
+find . -maxdepth 1 -type f -regextype posix-extended \
+                -regex '.*(LICENSE|README.rst|README.md|meson.build|\.(c|h|in|proto|txt|py))' \
+                -exec cp "{}" "${TMP_BUILD}" \;
+
+find scripts/ -maxdepth 1 -type f -regextype posix-extended \
+                -regex '.*(meson.build|.*\.py)' \
+                -exec cp --parents "{}" "${TMP_BUILD}" \;
+
+cp --parents startup/virtioforwarder "${TMP_BUILD}"
+cp --parents startup/systemd/*.service "${TMP_BUILD}"
+cp --parents startup/systemd/meson* "${TMP_BUILD}"
+cp -r doc/ "${TMP_BUILD}"
+
+find startup/ -maxdepth 1 -type f -regextype posix-extended \
+                -regex '.*(meson.build|.*\.sh)' \
+                -exec cp --parents "{}" "${TMP_BUILD}" \;
+
+# Grab the correct version string from vrelay_version.h
+cd "${MESON_BUILD_ROOT}"/_build
+VERSION_VER_STRING=$(awk '/VIRTIO_FWD_VERSION/&&/define/&&!/SHASH/{count++;if (count<4) printf "%s.", $3; else print $3}' \
+    "${MESON_BUILD_ROOT}"/vrelay_version.h)
+
+# Populate spec file
+cp "${MESON_SOURCE_ROOT}"/packaging/virtio-forwarder.spec.in \
+    "${RPM_TOPDIR}"/SPECS/virtio-forwarder.spec
+sed -ri "s/__VRELAY_VERSION__/${VERSION_VER_STRING}/g" \
+    "${RPM_TOPDIR}"/SPECS/virtio-forwarder.spec
+sed -ri "s/__PKG_RELEASE__/${PKG_RELEASE}/g" \
+    "${RPM_TOPDIR}"/SPECS/virtio-forwarder.spec
+sed -ri "s/__DATE__/${DATE_STR}/g" \
+    "${RPM_TOPDIR}"/SPECS/virtio-forwarder.spec
+sed -ri "s/__STATIC__/${STATIC}/g" \
+    "${RPM_TOPDIR}"/SPECS/virtio-forwarder.spec
+sed -ri "s/__CONFLICTS_WITH__/${CONFLICTS_WITH}/g" \
+    "${RPM_TOPDIR}"/SPECS/virtio-forwarder.spec
+sed -ri "s/__MESON_STATIC__/${MESON_STATIC}/g" \
+    "${RPM_TOPDIR}"/SPECS/virtio-forwarder.spec
+
+# Finally, generate a .tar.bz2
+VIRTIO_FORWARDER_NAME=virtio-forwarder"${STATIC}"-"${VERSION_VER_STRING}"
+VIRTIO_FORWARDER_BZ2="${VIRTIO_FORWARDER_NAME}"-"${PKG_RELEASE}".tar.bz2
+mv virtio-forwarder/ "${VIRTIO_FORWARDER_NAME}"
+tar cfjp "${RPM_TOPDIR}"/SOURCES/$VIRTIO_FORWARDER_BZ2 "${VIRTIO_FORWARDER_NAME}"
+
+# Call rpmbuild
+rpmbuild -ba -D "_topdir "${RPM_TOPDIR}"" "${RPM_SPEC}"
+
+# Copy the final RPM to $outdir
+if [ -z "$OUTDIR" ]
+then
+    echo 'Error: $OUTDIR is not defined'
+    exit 1
+else
+    find "${RPM_TOPDIR}" -name "*.rpm" -exec cp {} "$OUTDIR" \;
+fi
+
+exit 0

virtio-forwarder.spec.in
@@ -1,33 +1,54 @@
-Name:           virtio-forwarder
+Name:           virtio-forwarder__STATIC__
 Version:        __VRELAY_VERSION__
 Release:        __PKG_RELEASE__%{?dist}
 Summary:        SR-IOV virtual function to virtio packet forwarder
 
 License:        BSD
 URL:            http://netronome.com
-Source0:        https://rpm.netronome.com/archives/virtio-forwarder/virtio-forwarder-__VRELAY_VERSION__-__PKG_RELEASE__.tar.bz2
-%if 0%{?rhel} && 0%{?rhel} <= 7
-Requires:       protobuf-python
+Source0:        https://rpm.netronome.com/archives/virtio-forwarder/virtio-forwarder__STATIC__-__VRELAY_VERSION__-__PKG_RELEASE__.tar.bz2
+
+## Fedora Requires and BuildRequires
+%if 0%{?fedora}
+Requires:       python3-protobuf
 Requires:       python-zmq
-%else
+BuildRequires:  python-devel
+BuildRequires:  python-sphinx
+BuildRequires:  python-sphinx_rtd_theme
+%endif
+
+## Rhel/CentOS 7.x
+%if 0%{?rhel} && 0%{?rhel} >= 7 && 0%{?rhel} < 8
 Requires:       python2-protobuf
 Requires:       python2-zmq
-%endif
-BuildRequires:  make gcc dpdk-devel
-BuildRequires:  protobuf-c-compiler protobuf-c-devel protobuf-compiler
-BuildRequires:  zeromq-devel
-BuildRequires:  python2-devel python2-sphinx
-%if 0%{?rhel}
-BuildRequires:  python-sphinx_rtd_theme
-%else
+BuildRequires:  python2-devel
+BuildRequires:  python2-sphinx
 BuildRequires:  python2-sphinx_rtd_theme
 %endif
+
+## Rhel/CentOS 8.x
+%if 0%{?rhel} && 0%{?rhel} >= 8
+Requires:       python3-protobuf
+Requires:       python3-zmq
+BuildRequires:  python3-devel
+BuildRequires:  python3-sphinx
+BuildRequires:  python3-sphinx_rtd_theme
+%endif
+
+## Common Requires and BuildRequires
+BuildRequires:  meson
+BuildRequires:  make gcc dpdk-devel
+# Note, for CentOS 8.x the 'PowerTools' repo needs to be enabled for these
+BuildRequires:  protobuf-c-compiler protobuf-c-devel protobuf-compiler
+BuildRequires:  zeromq-devel
 BuildRequires:  numactl-devel
 %{?systemd_requires}
 BuildRequires:  systemd
 
 # Track DPDK's architectures
 ExclusiveArch: x86_64 aarch64 ppc64le %ix86
+
+# This gets populated when building n statically linked virtio-forwarder package
+__CONFLICTS_WITH__
 
 %description
 virtio-forwarder is a userspace networking application that forwards
@@ -40,19 +61,12 @@ of the DPDK poll mode driver (PMD) mechanism.
 %setup -q
 
 %build
-# We need to clean the variables that may have been set from a previous
-# invocation of make, e.g. if rpmbuild is called from a Makefile.
-env -i PATH=$PATH ${RTE_SDK:+RTE_SDK=$RTE_SDK} \
-	${RTE_TARGET:+RTE_TARGET=$RTE_TARGET} \
-	${CFLAGS:+CFLAGS="$CFLAGS"} ${V:+V="$V"} \
-	%make_build
+%undefine _annotated_build
+%meson __MESON_STATIC__
+%meson_build
 
 %install
-env -i PATH=$PATH ${RTE_SDK:+RTE_SDK=$RTE_SDK} \
-	${RTE_TARGET:+RTE_TARGET=$RTE_TARGET} \
-	${CFLAGS:+CFLAGS="$CFLAGS"} ${V:+V="$V"} \
-	%{make_install} prefix=%{_prefix} unitdir=%{_unitdir} \
-	libexecdir=%{_libdir}/virtio-forwarder
+%meson_install
 
 %post
 %systemd_post virtio-forwarder.service

commit a9e8ad287145c593e209713e5bd06a32ff10d30c

Propagate Debian/Ubuntu codename when building .deb packages
Currently the Debian/Ubuntu codename is only passed to debuild when
creating source packages. This behavior is also desirable when building
.deb packages. Built packages will be copied to the specified output
directory.

Fixes: 621eab1 ("Add meson build support")
Change-Id: I7ed84818cc7eebda610a234b58822dcc43bfe64f
Signed-off-by: Heinrich Kuhn <heinrich.kuhn@netronome.com>
Reviewed-by: Louis Peens <louis.peens@netronome.com>
Signed-off-by: Simon Horman <simon.horman@netronome.com>

meson.build
@@ -60,10 +60,16 @@ endif
 if get_option('deb')
     prep_run_debuild = find_program(join_paths('scripts', 'prep_and_run_debuild.sh'))
 
-    if get_option('static')
-        run_target('build_deb', command: [prep_run_debuild, '-s'])
+    if get_option('outdir') == 'outdir'
+        error('Please provide destination for DEB via the "-Doutdir" option')
     else
-        run_target('build_deb', command: [prep_run_debuild])
+        outdir = get_option('outdir')
+    endif
+
+    if get_option('static')
+        run_target('build_deb', command: [prep_run_debuild, '-s', '-o', outdir, '-d', get_option('debian_dist')])
+    else
+        run_target('build_deb', command: [prep_run_debuild, '-d', '-o', outdir, get_option('debian_dist')])
     endif
 endif
 

scripts/prep_and_run_debuild.sh
@@ -31,7 +31,7 @@
 #   (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 #   OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-while getopts 'smd:' opt; do
+while getopts 'smo:d:' opt; do
     case ${opt} in
       s)
         STATIC="-static"
@@ -40,6 +40,9 @@ while getopts 'smd:' opt; do
         ;;
       m)
         SKIP_DEBUILD="true"
+        ;;
+      o)
+        OUTDIR="$OPTARG"
         ;;
       d)
         DEBIAN_DISTRO=${OPTARG}
@@ -148,4 +151,14 @@ cat << EOF
 EOF
 else
   debuild --rootcmd=fakeroot -e PATH -e CFLAGS -e V -us -uc
+
+  # Copy the final DEB to $outdir
+  if [ -z "$OUTDIR" ]
+  then
+      echo 'Error: $OUTDIR is not defined'
+      exit 1
+  else
+      find "${MESON_BUILD_ROOT}"/_build -name "*.deb" -exec cp {} "$OUTDIR" \;
+  fi
+
 fi